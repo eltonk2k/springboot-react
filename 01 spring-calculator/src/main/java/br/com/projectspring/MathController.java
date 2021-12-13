4cfd1b26ed712faed71fd824aaf5d86e96228314
eltonk2k <elieltonpb1@gmail.com>
2021-12-12 21:11:08 -0300

    Executando o Projeto

--- 01 spring-calculator/src/main/java/br/com/projectspring/MathController.java
+++ 01 spring-calculator/src/main/java/br/com/projectspring/MathController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MathController {
 	
-	@RequestMapping(value="/sum/numberOne/numberTwo", method=RequestMethod.GET)
+	@RequestMapping(value="/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
 	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
 		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
 			throw new Exception();
@@ -17,14 +17,17 @@ public class MathController {
 		return sum;
 	}
 
-	private Double convertToDouble(String numberTwo) {
-		// TODO Auto-generated method stub
-		return 1D;
+	private Double convertToDouble(String strNumber) {
+		if (strNumber == null) return 0D;
+		String number = strNumber.replaceAll(",", ".");
+		if (isNumeric(number)) return Double.parseDouble(number);
+		return 0D;
 	}
 
-	private boolean isNumeric(String number) {
-		// TODO Auto-generated method stub
-		return false;
+	private boolean isNumeric(String strNumber) {
+		if (strNumber == null) return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
 	}
 
 }
