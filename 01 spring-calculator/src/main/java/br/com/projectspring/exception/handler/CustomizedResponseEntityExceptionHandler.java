51e3c9f764e5adab8e398562554e098ed8c7d766
eltonk2k <elieltonpb1@gmail.com>
2021-12-15 22:51:46 -0300

    Ajustando o Service

--- 01 spring-calculator/src/main/java/br/com/projectspring/exception/handler/CustomizedResponseEntityExceptionHandler.java
+++ 01 spring-calculator/src/main/java/br/com/projectspring/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 
 import br.com.projectspring.exception.ExceptionResponse;
-import br.com.projectspring.exception.UnsuportedMathOperationException;
+import br.com.projectspring.exception.ResourceNotFoundException;
 
 @ControllerAdvice
 @RestController
@@ -23,7 +23,7 @@ public class CustomizedResponseEntityExceptionHandler {
 	}
 	
 	
-	@ExceptionHandler(UnsuportedMathOperationException.class)
+	@ExceptionHandler(ResourceNotFoundException.class)
 	public final ResponseEntity<ExceptionResponse> handleBadRequestExeption(Exception ex, WebRequest request) {
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
