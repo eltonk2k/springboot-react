70bcdceb8d7054320288d8685f1dd60032f58e89
eltonk2k <elieltonpb1@gmail.com>
2021-12-18 22:12:49 -0300

    Criando um Custom Converter

--- 01 spring-calculator/src/main/java/br/com/projectspring/controller/PersonController.java
+++ 01 spring-calculator/src/main/java/br/com/projectspring/controller/PersonController.java
@@ -7,10 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projectspring.data.vo.PersonVO;
@@ -18,46 +18,45 @@ import br.com.projectspring.data.vo.v2.PersonVOV2;
 import br.com.projectspring.services.PersonServices;
 
 @RestController
-@RequestMapping("/person/")
+@RequestMapping("/person")
 public class PersonController {
 	
 	@Autowired
-	private PersonServices services;
+	private PersonServices service;
 	
 	
 	@GetMapping
 	public List<PersonVO> findAll() {
-		return services.findAll();
+		return service.findAll();
 	}
 	
 	
 	@GetMapping("/{id}")
 	public PersonVO findById(@PathVariable("id") Long id) {
-		return services.findById(id);
+		return service.findById(id);
 		
 	}
 	
-	@RequestMapping(method=RequestMethod.POST)
+	@PostMapping
 	public PersonVO create(@RequestBody PersonVO person) {
-		return services.create(person);
+		return service.create(person);
 		
 	}
 	
-	@RequestMapping(value = "/v2", method=RequestMethod.POST)
+	@PostMapping("/v2")
 	public PersonVOV2 createV2(@RequestBody PersonVOV2 person) {
-		return services.createV2(person);
-		
+		return service.createV2(person);
 	}
 		
 	@PutMapping
 	public PersonVO update(@RequestBody PersonVO person) {
-		return services.update(person);
+		return service.update(person);
 		
 	}
 	
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
-		services.delete(id);
+		service.delete(id);
 		return ResponseEntity.ok().build();
 	}
 	
--- 01 spring-calculator/src/main/java/br/com/projectspring/services/PersonServices.java
+++ 01 spring-calculator/src/main/java/br/com/projectspring/services/PersonServices.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.projectspring.converter.DozerConverter;
+import br.com.projectspring.converter.custom.PersonConverter;
 import br.com.projectspring.data.model.Person;
 import br.com.projectspring.data.vo.PersonVO;
 import br.com.projectspring.data.vo.v2.PersonVOV2;
@@ -18,17 +19,22 @@ public class PersonServices {
 	@Autowired
 	PersonRepository repository;
 	
+	@Autowired
+	PersonConverter converter;
+	
 	public PersonVO create(PersonVO person) {
 		var entity = DozerConverter.parseObject(person, Person.class);
 		var vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
 		return vo;
 	}
 	
+	
 	public PersonVOV2 createV2(PersonVOV2 person) {
-		var entity = DozerConverter.parseObject(person, Person.class);
-		var vo = DozerConverter.parseObject(repository.save(entity), PersonVOV2.class);
+		var entity = converter.convertVOToEntity(person);
+		var vo = converter.convertEntityToVO(repository.save(entity));
 		return vo;
 	}
+	
 	
 	public List<PersonVO> findAll() {
 		return DozerConverter.parseListObjects(repository.findAll(), PersonVO.class);
