bb9e2a16ec6699f7d0d417f47858f1a04c24e959
eltonk2k <elieltonpb1@gmail.com>
2021-12-22 23:12:34 -0300

    Migrations com Flyway via Plugin no POM

--- 01 spring-calculator/src/main/resources/db/migration/V1_Create_Table_Person.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `person` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(80) NOT NULL,
-  `last_name` varchar(80) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `address` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `gender` varchar(6) NOT NULL,
-  PRIMARY KEY (`id`)
-)
--- 01 spring-calculator/src/main/resources/db/migration/V2_Populate_Table_Person.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUES
-	(1, 'João', 'Gomes', 'Bayeux - Paraiba - Brasil', 'Male'),
-	(2, 'Grabriela', 'Silva', 'Bayeux - Paraiba - Brasil', 'Famale'),
-	(5, 'Pedro', 'Fé', 'Bayeux - Paraiba - Brasil', 'Male'),
-	(7, 'Fernanda', 'Pereira', 'Bayeux - Paraiba - Brasil', 'Female'),
-	(8, 'Ronaldo', 'Santos', 'Bayeux - Paraiba - Brasil', 'Male'),
-	(9, 'Marcos', 'Bras', 'Bayeux - Paraiba - Brasil', 'Male');
