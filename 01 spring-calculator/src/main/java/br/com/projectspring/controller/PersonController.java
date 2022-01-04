6e6c3b753a63e35d28865f324a11959d488f581e
eltonk2k <elieltonpb1@gmail.com>
2022-01-04 09:31:41 -0300

    Habilitando CORS no Spring Boot

--- 01 spring-calculator/src/main/java/br/com/projectspring/controller/PersonController.java
+++ 01 spring-calculator/src/main/java/br/com/projectspring/controller/PersonController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import br.com.projectspring.services.PersonServices;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
+//@CrossOrigin
 @Api(tags = "Person Endpoint")
 @RestController
 @RequestMapping("/api/person/v1")
