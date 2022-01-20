f0312bd48ea8d641a858964dde422780239bbf4f
eltonk2k <elieltonpb1@gmail.com>
2022-01-19 23:17:25 -0300

    Preparando o Servico para Prover Downloads

--- 01 springProject/src/main/java/br/com/projectspring/services/FileStorageService.java
+++ 01 springProject/src/main/java/br/com/projectspring/services/FileStorageService.java
@@ -6,12 +6,15 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.projectspring.config.FileStorageConfig;
 import br.com.projectspring.exception.FileStorageException;
+import br.com.projectspring.exception.MyFileNotFoundException;
 
 @Service
 public class FileStorageService {
@@ -43,7 +46,21 @@ public class FileStorageService {
 		} catch (Exception e) {
 			throw new FileStorageException("Could not store file " + fileName + ". Please try again!", e);
 		}
-		
+	}
+	
+	public Resource loadFileAsResource(String fileName) {
+		try {
+			Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+			Resource resource = new UrlResource(filePath.toUri());
+			if (resource.exists()) {
+				return resource;
+			} else {
+				throw new MyFileNotFoundException("File not file " + fileName);
+			}
+			
+		} catch (Exception e) {
+			throw new MyFileNotFoundException("File not file " + fileName, e);
+		}
 	}
 	
 }
