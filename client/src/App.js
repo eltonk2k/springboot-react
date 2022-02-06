ea76a3f4e2021c81e44814144a834c8c82f571f1
eltonk2k <elieltonpb1@gmail.com>
2022-02-05 21:41:15 -0300

    Conceito de estado e imutabilidade

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './Header';
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <Header>
-      Client REST Udemy - Properties
-    </Header>
+    <div>
+      <Header>
+        Counter: {counter}
+      </Header>
+      <button onClick={increment}>Add</button>
+    </div>
+    
   );
 }
